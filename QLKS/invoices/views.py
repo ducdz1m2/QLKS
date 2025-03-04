1f51ebf86ee706d54333eed069bfcd1e663ae083
ducdz1m2 <93931868+ducdz1m2@users.noreply.github.com>
2025-03-04 20:28:27 +0700

    update views

--- QLKS/invoices/views.py
+++ QLKS/invoices/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from.models import HoaDon
 # Create your views here.
 
+
+
 def invoices_list(request):
     invoice = HoaDon.objects.all()
     return render(request, 'invoices/invoices_list.html', {'invoices': invoice})
