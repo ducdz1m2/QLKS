455e8550181a907738e07797759db68cd6ae42db
Code002 <kakashivncm123@gmail.com>
2025-03-05 14:54:25 +0700

    Update App Customer

--- QLKS/customer/urls.py
+++ QLKS/customer/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import customer_list, rentroom_list
+from .views import *
 
 urlpatterns = [
     path('', customer_list, name='customer_list'),
     path('rentroom/', rentroom_list, name='rentroom_list'),
+    path('add/customer/', add_customer, name='add_customer'),
+    path('edit/customer/<int:MaKhachHang>', edit_customer, name='edit_customer'),
+    path('delete/customer/<int:MaKhachHang>', delete_customer, name='delete_customer'),
+    path('detail/customer/<int:MaKhachHang>', detail_customer, name='detail_customer'),
 ]
--- QLKS/customer/views.py
+++ QLKS/customer/views.py
@@ -1,24 +1,80 @@
-from django.shortcuts import render
-from .models import KhachHang, ThuePhong
+from django.shortcuts import render, redirect
+from .models import *
 from django.db import connection
+from django.contrib import messages
 
-# Create your views here.
+# Thêm khách hàng
+def add_customer(request):
+    if request.method == "POST":
+        TenKhachHang = request.POST['TenKhachHang']
+        DiaChi = request.POST['DiaChi']
+        SoDienThoai = request.POST['SoDienThoai']
+        with connection.cursor() as cursor:
+            cursor.callproc('AddCustomer', [TenKhachHang, DiaChi, SoDienThoai])
+        messages.success(request, f"Thêm khách hàng {TenKhachHang} thành công!")
+    return render(request, 'customer/add_customer.html')
+
+# Lấy một khách hàng theo mã 
+def get_customer(MaKhachHang):
+    with connection.cursor() as cursor:
+        cursor.callproc("GetCustomer", [MaKhachHang])
+        row = cursor.fetchone()
+        columns = [col[0] for col in cursor.description] if cursor.description else []
+        return dict(zip(columns, row)) if row else None
+    
+# Lấy ra tất cả khách hàng
 def get_all_customers():
     with connection.cursor() as cursor:
         cursor.callproc("GetAllCustomer")
         columns = [col[0] for col in cursor.description]  # Lấy tên cột
         return [dict(zip(columns, row)) for row in cursor.fetchall()]  # Convert tuple -> dict
 
+# Show thông tin 1 khách hàng   
+def detail_customer(request, MaKhachHang):  
+    customer = get_customer(MaKhachHang) 
+    if not customer:
+        return render(request, 'customer/detail_customer.html', {'error': 'Không tìm thấy khách hàng'})
+    return render(request, 'customer/detail_customer.html', {'customer' : customer})
+
+# Chỉnh sửa thông tin 1 khách hàng
+def edit_customer(request, MaKhachHang):
+    customer = get_customer(MaKhachHang)    
+    
+    if request.method == 'POST':
+        TenKhachHang = request.POST.get("TenKhachHang")
+        DiaChi = request.POST.get("DiaChi")
+        SoDienThoai = request.POST.get("SoDienThoai")
+        with connection.cursor() as cursor:
+            cursor.callproc('UpdateCustomer', [MaKhachHang ,TenKhachHang, DiaChi, SoDienThoai])
+        messages.success(request, f"Cập nhật khách hàng {TenKhachHang} thành công!")
+        return redirect('customer_list')
+    
+    if not customer:
+        return render(request, 'customer/edit_customer.html', {'error': 'Không tìm thấy khách hàng'})
+    return render(request, 'customer/edit_customer.html', {'customer' : customer})
+
+# Xóa khách hàng
+def delete_customer(request, MaKhachHang):
+    customer = get_customer(MaKhachHang)  
+    if request.method == 'POST':  
+        with connection.cursor() as cursor:
+            cursor.callproc('DeleteCustomer', [MaKhachHang])
+        messages.success(request, f"Xóa khách hàng {customer.TenKhachHang} thành công!")
+    return redirect('customer_list')
+
+# Hiện danh sách khách hàng
 def customer_list(request):
     customers = get_all_customers()
     return render(request, 'customer/customer_list.html', {'customers': customers})
 
+# Lấy tất cả thông tin thuê phòng
 def get_all_rentrooms():
     with connection.cursor() as cursor:
         cursor.callproc("GetAllRentRoom")
         columns = [col[0] for col in cursor.description]  # Lấy tên cột
         return [dict(zip(columns, row)) for row in cursor.fetchall()]  # Convert tuple -> dict
-        
+ 
+# Hiện danh sách thông tin thuê phòng        
 def rentroom_list(request):
     rentrooms = get_all_rentrooms()
     print(rentrooms)
