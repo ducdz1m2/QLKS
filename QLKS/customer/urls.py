fa2ac10bb0aea09a6433ce514cea7a98d810c1b4
Code002 <kakashivncm123@gmail.com>
2025-03-06 13:59:30 +0700

    Update Url Customer

--- QLKS/customer/urls.py
+++ QLKS/customer/urls.py
@@ -3,9 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', customer_list, name='customer_list'),
-    path('rentroom/', rentroom_list, name='rentroom_list'),
-    path('add/customer/', add_customer, name='add_customer'),
-    path('edit/customer/<int:MaKhachHang>', edit_customer, name='edit_customer'),
-    path('delete/customer/<int:MaKhachHang>', delete_customer, name='delete_customer'),
-    path('detail/customer/<int:MaKhachHang>', detail_customer, name='detail_customer'),
+    path('add/', add_customer, name='add_customer'),
+    path('edit/<int:MaKhachHang>', edit_customer, name='edit_customer'),
+    path('delete/<int:MaKhachHang>', delete_customer, name='delete_customer'),
+    path('detail/<int:MaKhachHang>', detail_customer, name='detail_customer'),
 ]
