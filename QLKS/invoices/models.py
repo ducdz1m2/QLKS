f6a4e77a917c0d9f6b01b3fdae27f70f6f0b75e1
DuongHuuLuan <luanb2205946@student.ctu.edu.vn>
2025-03-06 11:34:14 +0700

    Upadate invoices

--- QLKS/invoices/models.py
+++ QLKS/invoices/models.py
@@ -1,9 +1,12 @@
 from django.db import models
-
+from service.models import SuDungDichVu
 # Create your models here.
 
 class HoaDon(models.Model):
     MaHoaDon = models.AutoField(primary_key=True)
-    MaSDDV = models.IntegerField()
+    MaSuDung = models.ForeignKey(SuDungDichVu, on_delete=models.CASCADE,verbose_name="Mã Sử Dụng",  default=1)
     NgayLapHoaDon = models.DateField()
     TongTien = models.DecimalField(max_digits=15, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.MaHoaDon} - {self.MaSuDung} - {self.NgayLapHoaDon} - {self.TongTien}"
