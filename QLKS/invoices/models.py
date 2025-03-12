282378d7afdab85089c977a145ba138ea312e967
ducdz1m2 <93931868+ducdz1m2@users.noreply.github.com>
2025-03-12 09:54:53 +0700

    update hoadon

--- QLKS/invoices/models.py
+++ QLKS/invoices/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from service.models import SuDungDichVu
+from customer.models import ThuePhong
 # Create your models here.
 
 class HoaDon(models.Model):
     MaHoaDon = models.AutoField(primary_key=True)
-    MaSuDung = models.ForeignKey(SuDungDichVu, on_delete=models.CASCADE,verbose_name="Mã Sử Dụng",  default=1)
+    MaSuDung = models.ForeignKey(ThuePhong, on_delete=models.CASCADE, default=1)
     NgayLapHoaDon = models.DateField()
     TongTien = models.DecimalField(max_digits=15, decimal_places=2)
 
